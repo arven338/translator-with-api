5f885d6b13d5cce86c8d94c6f463a663c2959297
Arven <arven330@proton.me>
2025-10-28 11:13:33 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,10 +1,13 @@
-import translators as ts
+import translators as ts # Import library
 
+# Source translator class
 class AI_Translator:
+  # Constructor with default-parameters
   def __init__(self, source_language='auto', target_language='en'):
     self.source_language = source_language
     self.target_language = target_language
 
+  # Base translate function
   def translate(self, text):
     try:
       translated = ts.translate_text(query_text=text, translator='google', from_language=self.source_language, to_language=self.target_language)
